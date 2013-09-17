bb1679eff97e00b014513a35fa15e04fe8dda9ef
netpoetica <krosenberg@kbsp.com>
2013-09-17 01:37:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Retina Image View
+
+The goal of this very small web app is to simply allow a user to see a retina image (@2x) and regular image stacked side-by-side.
+
+# Resources
+## Designing for iPhone 4 Retina Display: Techniques and Workflow
+http://mobile.smashingmagazine.com/2010/11/17/designing-for-iphone-4-retina-display-techniques-and-workflow/
+
+"Building apps for the iPhone 4 Retina display involves creating two sets of images — one at 163 ppi and another at 326 ppi. The 326 ppi images include @2x at the end of their filename, to denote that they’re double the resolution."
+
+## Creating Retina Images for Your Website
+http://www.kylejlarson.com/blog/2012/creating-retina-images-for-your-website/
+More of a design guide, but short and to the point - helpful in discussing retina app icon sizes for iPhone apps.
+
+
+## Towards Retina Web
+http://coding.smashingmagazine.com/2012/08/20/towards-retina-web/
+Super thorough article on retina - explains differntiation in pixels amongst browsers, and many other things - it gets into more detail than a computer science book. From very reliable resource and author.
---
+++ static/js/main.js
@@ -0,0 +1,95 @@
+var App = function($){
+  console.log("-> App::new()");
+
+  // Save reference to App object scope
+  var _this = this;
+  var IMAGE_URL = 'static/images/';
+
+  // IDs of various elements requiring JS attention.
+  var ElementsVO = [
+    'non-retina-img',
+    'retina-img',
+    'non-retina-bg',
+    'retina-bg'
+  ];
+
+  var resizeBgContainer = function(){
+    var _this = this;
+    var selector = $(this).attr('id');
+
+    // Resize all containers to the same size as original. This is the point of
+    // high res images.
+    if(selector == 'non-retina-img'){
+      $.each(ElementsVO, function(){
+        if(this.indexOf('bg') > -1){
+          // It's a background image, div w a bg img
+          var opts = {
+            width: $(_this).width(),
+            height: $(_this).height()
+          };
+
+          // If it's CSS bg and retina it needs a background-size.
+          if(this.indexOf('retina') > -1){
+            opts['background-size'] = $(_this).width() + 'px ' + $(_this).height() + 'px'
+          }
+
+          $('#' + this).css(opts);
+
+        } else {
+          // Otherwise it's an image tag.
+          $('#' + this).attr({
+            width: $(_this).width(),
+            height: $(_this).height()
+          });
+        }
+      });
+    }
+  };
+
+  // Single point of entry - called immediately after instantiation
+  function _init(){
+     console.log("-> App::init()");
+
+     $.getJSON('/config.json', function(res){
+       $.each(ElementsVO, function(){
+         var fullPath = IMAGE_URL + res.fileName;
+
+         if(this.indexOf('non-retina') == -1){
+           // This one should not be "@2x'
+           fullPath += '@2x';
+         }
+
+         // Add the fileType extension.
+         fullPath += ('.' +  res.fileType);
+
+         if(this.indexOf('img') != -1){
+           // Not a background image
+           $('#' + this)
+           .load(resizeBgContainer)
+           .attr('src', fullPath);
+         } else {
+           console.log("Found bg image: " + this)
+           $('#' + this).css('background-image', 'url(' + fullPath + ')')
+         }
+       });
+     });
+
+  }
+
+  // Application methods
+
+  /* ************************************************************************** */
+  /* At the end of App instantiation, call the init function of the App object. */
+  /* ************************************************************************** */
+  _init.call(this);
+};
+
+// Override the toString method for more productive error reporting.
+App.prototype.toString = function(){
+  return '[object App]';
+};
+
+// Instantiate application in $(document).ready()
+$(document).ready(function(){
+  window.app = new App(window.jQuery);
+});
